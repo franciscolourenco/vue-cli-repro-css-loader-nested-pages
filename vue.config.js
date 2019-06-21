a5509beb6349efbb92ca27c0e2c25cc51876dbf0
Francisco Lourenco <aristidesfl@gmail.com>
2019-06-21 12:45:27 +0200

    Reproduce

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pages: {
+    'nestedpage1/index': {
+      entry: 'src/nestedpage1/main.js',
+    },
+    'nestedPpage2/index': {
+      entry: 'src/nestedpage2/main.js',
+    },
+  }
+}
